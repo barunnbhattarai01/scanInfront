d06dd758fa110ff60bb3a747927e73af40197ea3
Darwin Koirala <darwinkoirala123@gmail.com>
2025-07-18 15:45:19 +0545

    changed config

--- src/configuration.js
+++ src/configuration.js
@@ -1,2 +1,2 @@
-//export const BACKENDURL = "https://scanin-production.up.railway.app"
-export const BACKENDURL = "http://localhost:4000"
+export const BACKENDURL = "https://scanin-production.up.railway.app"
+//export const BACKENDURL = "http://localhost:4000"
