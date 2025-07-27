6896daf5a3a81259cf87ac9c1e9aa595712acd7c
Nirmal Chhetri <chhetrinirmal765@gmail.com>
2025-07-27 22:03:54 +0545

    changed the qr scanning package

--- src/features/qr/page/ScanQr.jsx
+++ src/features/qr/page/ScanQr.jsx
@@ -61,28 +61,21 @@ function ScanQr() {
     if (respose.ok) {
       setMessage({
         type: "success",
-        text: (
-          <span className="flex items-center gap-2">
-            <CheckCircle className="w-5 h-5 text-green-600 " />
-            Successfully Checked-in!
-          </span>
-        ),
+        text: "Qr successfully scanned",
       });
       return;
+    } else if (respose.status === 409) {
+      setMessage({
+        type: "twice",
+        text: "Same qr cannot be scanned twice",
+      });
     } else {
       setMessage({
-        type: "error",
-        text: (
-          <span className="flex items-center gap-2">
-            <XCircle className="w-5 h-5 text-red-600" />
-            Check-in Failed!
-          </span>
-        ),
+        type: "invalid",
+        text: "The qr is invalid",
       });
-      return;
     }
   }
-
   return (
     <div
       style={{
@@ -94,25 +87,27 @@ function ScanQr() {
         padding: "20px",
       }}
     >
-      <QRScanner onScanSuccess={(text) => onClick(text)} />
+      {!message && <QRScanner onScanSuccess={(text) => onClick(text)} />}
 
       {message &&
-        (message.type == "success" ? (
+        (message?.type == "success" ? (
           <div>
-            <div class="success-checkmark">
-              <div class="check-icon">
-                <span class="icon-line line-tip"></span>
-                <span class="icon-line line-long"></span>
-                <div class="icon-circle"></div>
-                <div class="icon-fix"></div>
+            <div className="min-h-80 flex flex-col justify-center items-center">
+              <div class="success-checkmark">
+                <div class="check-icon">
+                  <span class="icon-line line-tip"></span>
+                  <span class="icon-line line-long"></span>
+                  <div class="icon-circle"></div>
+                  <div class="icon-fix"></div>
+                </div>
               </div>
+              <p className="font-black text-green-400 text-xl capitalize">
+                Qr scanning was successfull
+              </p>
             </div>
-            <center>
-              <button id="restart">Restart Animation</button>
-            </center>
           </div>
-        ) : (
-          <div>
+        ) : message?.type == "twice" ? (
+          <div className="min-h-80 flex justify-center items-center flex-col">
             <svg
               class="crossmark addClass"
               xmlns="http://www.w3.org/2000/svg"
@@ -136,6 +131,40 @@ function ScanQr() {
                 d="M16,36 l20,-20"
               />
             </svg>
+
+            <p className="font-black text-red-400 text-xl capitalize">
+              Same qr cannot be scanned twice
+            </p>
+          </div>
+        ) : (
+          <div className="min-h-80 flex justify-center items-center flex-col">
+            <svg
+              class="crossmark addClass"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 52 52"
+            >
+              <circle
+                class="crossmark__circle addClass"
+                cx="26"
+                cy="26"
+                r="25"
+                fill="none"
+              />
+              <path
+                class="cross__path cross__path--right addClass"
+                fill="none"
+                d="M16,16 l20,20"
+              />
+              <path
+                class="cross__path cross__path--left addClass"
+                fill="none"
+                d="M16,36 l20,-20"
+              />
+            </svg>
+
+            <p className="font-black text-red-400 text-xl capitalize">
+              The Provided qr is invalid
+            </p>
           </div>
         ))}
     </div>
